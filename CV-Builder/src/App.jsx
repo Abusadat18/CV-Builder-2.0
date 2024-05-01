6de4eebad7e83159a7f7ae5772dcecc2d379dc21
Abu Sadat Ansari <itssadatansari@gmail.com>
2024-05-01 08:29:10 +0530

    Make Header component

--- CV-Builder/src/App.jsx
+++ CV-Builder/src/App.jsx
@@ -1,12 +1,15 @@
+import { useState } from 'react'
 import './App.css'
 import {Header} from './components'
 
 
 function App() {
 
+  const [isEditable, setIsEditable] = useState(true)
+
   return (
     <>
-      <Header/>
+      <Header isEditable={isEditable} setIsEditable={setIsEditable}/>
     </>
   )
 }
--- CV-Builder/src/components/Header.jsx
+++ CV-Builder/src/components/Header.jsx
@@ -1,8 +1,46 @@
-import React from 'react'
+import React, { useState } from 'react'
+import "../styles/Header.css"
 
-function Header() {
+function Header({isEditable, setIsEditable}) {
+
+  const [name, setName] = useState("")
+  const [email, setEmail] = useState("")
+  const [address, setAddress] = useState("")
+  const [phNum, setPhNum] = useState("")  
+
   return (
-    <div>Header</div>
+    <header>
+        {isEditable ? (
+            <>
+                <div className='header-ctn1'>
+                    <input type="text"
+                        className='input-name' 
+                        placeholder='Name'
+                        value={name}
+                        onChange={(e) => setName(e.target.value)} 
+                    />
+                    <button className='toggle-btn' onClick={() => setIsEditable(!isEditable)}>Preview</button>
+                </div>
+                <div className='header-ctn2'>
+                    <input className='input-add' type="text" placeholder='Address' value={address} onChange={(e) => setAddress(e.target.value)}/>
+                    <input className='input-ph' type="tel" placeholder='Ph Number' value={phNum} onChange={(e) => setPhNum(e.target.value)}/>
+                    <input className='input-email' type="email" placeholder='Email'value={email} onChange={(e) => setEmail(e.target.value)}/>
+                </div>
+            </>
+        ) : (
+            <>
+               <div className='header-ctn1'>
+                    <h1>{name}</h1>
+                    <button className='toggle-btn' onClick={() => setIsEditable(!isEditable)}>Edit</button>
+                </div>
+                <div className='header-ctn2'>
+                    <h2>{address}</h2>
+                    <h2>{phNum}</h2>
+                    <h2>{email}</h2>
+                </div> 
+            </>
+        ) }
+    </header>
   )
 }
 
